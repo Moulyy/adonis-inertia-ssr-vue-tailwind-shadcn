6839d16a24495b2ea5e8777f9d4730aca7648079
Theo Moulart <thmotech@gmail.com>
2025-02-19 22:35:37 +0100

    Initial commit

---
+++ inertia/app/app.ts
@@ -0,0 +1,31 @@
+/// <reference path="../../adonisrc.ts" />
+/// <reference path="../../config/inertia.ts" />
+
+import '../css/app.css'
+import { createSSRApp, h } from 'vue'
+import type { DefineComponent } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import { resolvePageComponent } from '@adonisjs/inertia/helpers'
+
+const appName = import.meta.env.VITE_APP_NAME || 'AdonisJS'
+
+createInertiaApp({
+  progress: { color: '#5468FF' },
+
+  title: (title) => `${title} - ${appName}`,
+
+  // name args is the first argument passed to the inertia route in route.ts file
+  // router.on('/').renderInertia('home') -------> home is the name argument
+  resolve: (name) => {
+    return resolvePageComponent(
+      `../pages/${name}.vue`,
+      import.meta.glob<DefineComponent>('../pages/**/*.vue')
+    )
+  },
+
+  setup({ el, App, props, plugin }) {
+    createSSRApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+})
